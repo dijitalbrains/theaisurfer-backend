6c8e54302172191adba94b800508cf8abe0ee2b5
Shahzaib <dev@dijitalbrains.org>
2026-01-19 16:20:37 +0500

    Port update.

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
     },
   });
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 3002;
   await app.listen(port);
 
   console.log(`\n🚀 Application is running on: http://localhost:${port}`);
